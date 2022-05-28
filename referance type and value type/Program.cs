bee6ef6bcd3aa33ec8cce1bb2259a09f067a74da
ketkikk <kulketki7@gmail.com>
2022-05-28 10:35:56 +0530

    first commit

---
+++ referance type and value type/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace referance_type_and_value_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //value type and reference type
+            int x = 8;
+            int y = 9;
+            Console.WriteLine("Before swapping \n a=" + x + " b = " + y);
+            Swap(ref x, ref y);
+            Console.WriteLine("Before swapping \n a=" + x + " b = " + y);
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("In swap method \n a=" + a + " b = " + b);
+        }
+    }
+}
